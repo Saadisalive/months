c61f94912d99538c40dffa0e2d5661ca563ed7cd
Saadisalive <snafiullah@gmail.com>
2025-04-19 10:10:24 +0500

    Add files via upload

---
+++ months name.py
@@ -0,0 +1,49 @@
+import calendar
+
+a = 2025
+a1 = 1
+print(calendar.month(a,a1))
+
+b = 2025
+b1 = 2
+print(calendar.month(b,b1))
+
+c = 2025
+c1 = 3
+print(calendar.month(c,c1))
+
+d = 2025
+d1 = 4
+print(calendar.month(d,d1))
+
+e = 2025
+e1 = 5
+print(calendar.month(e,e1))
+
+f = 2025
+f1 = 6
+print(calendar.month(f,f1))
+
+g = 2025
+g1 = 7
+print(calendar.month(g,g1))
+
+h = 2025
+h1 = 8
+print(calendar.month(h,h1))
+
+i = 2025
+i1 = 9
+print(calendar.month(i,i1))
+
+j = 2025
+j1 = 10
+print(calendar.month(j,j1))
+
+k = 2025
+k1 = 11
+print(calendar.month(k,k1))
+
+l = 2025
+l1 = 12
+print(calendar.month(l,l1))
